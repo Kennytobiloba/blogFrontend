984d3e7e279af489cf15a348b7a3b15ae5f0c68f
kennytobiloba <olaatunbikehinde@gmail.com>
2024-11-20 10:41:51 +0100

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   build: {
-    outDir: 'build', // Change this to the appropriate output directory
-    chunkSizeWarningLimit: 1000, // Optional, adjust the chunk size warning limit
+    chunkSizeWarningLimit: 2048, // Increase the limit to 2MB
   },
 })
