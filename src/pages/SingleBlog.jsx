d3aa6b0cd34b87fbee76767828a61d6e44b49386
kennytobiloba <olaatunbikehinde@gmail.com>
2024-11-20 14:54:03 +0100

    updated

--- src/pages/SingleBlog.jsx
+++ src/pages/SingleBlog.jsx
@@ -5,37 +5,48 @@ import SingleBlogCard from '../components/SingleBlogCard'
 import Comment from '../components/Comment'
 import Relatedblog from '../components/Relatedblog'
 
-
 const SingleBlog = () => {
-     const { id } = useParams()
-     console.log("id", id)
-     const {data: blog, isLoading , error} = useFetchBlogByIdQuery(id)
-     console.log("blog", blog)
-  return (
-    <div>
-        {isLoading && <div>Loading.... </div>}
-        {error && <div> Something went wrong{error}</div>}
+    const { id } = useParams()
 
+    // Log the ID to make sure it's being passed correctly
+    console.log("Blog ID:", id)
+
+    // Fetch blog data using the id from the URL
+    const { data: blog, isLoading, error } = useFetchBlogByIdQuery(id)
+
+    // Log the fetched blog data
+    console.log("Fetched blog:", blog)
+
+    if (!id) {
+        return <div>Invalid blog ID</div>
+    }
+
+    return (
         <div>
-            {
-                blog?.post && (
-                    <div className=' container mx-auto flex flex-col lg:flex-row mt-6 justify-between md:gap-12'>
-                       <div>
-                       <SingleBlogCard blog={blog?.post}/>
+            {/* Loading State */}
+            {isLoading && <div>Loading....</div>}
+
+            {/* Error State */}
+            {error && <div>Something went wrong: {error.message}</div>}
+
+            {/* Blog Content */}
+            {blog?.post ? (
+                <div className="container mx-auto flex flex-col lg:flex-row mt-6 justify-between md:gap-12">
+                    <div>
+                        <SingleBlogCard blog={blog?.post} />
                         <div>
-                            <Comment comments={blog?.comment}/>
-                        </div>
-                       </div>
-                        <div className='bg-white lg:w-full w-full '>
-                        <Relatedblog/>
+                            <Comment comments={blog?.comment} />
                         </div>
                     </div>
-                )
-            }
-           
+                    <div className="bg-white lg:w-full w-full">
+                        <Relatedblog blogId={id} /> {/* Assuming you want to pass the current blog ID */}
+                    </div>
+                </div>
+            ) : (
+                !isLoading && <div>No blog found for this ID</div> // Show if no blog is found
+            )}
         </div>
-    </div>
-  )
+    )
 }
 
 export default SingleBlog
