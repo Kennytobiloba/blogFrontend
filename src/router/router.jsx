1986d47221bf042a842dce354efa5cb2b50795eb
kennytobiloba <olaatunbikehinde@gmail.com>
2024-11-20 10:12:35 +0100

    updated

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import Home from "../pages/home";
+import Home from "../pages/Home";
 import About from "../pages/About";
 import Privacy from "../pages/Privacy";
 import Contact from "../pages/Contact";
